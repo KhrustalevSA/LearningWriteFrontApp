b23a7842670116b49e8a22d640a9d259198f7429
serega <serega.135246@mail.ru>
2023-03-31 19:29:03 +0300

    Загрузка половины проекта на Гит

---
+++ tryWebFromEmptyProject/src/com/simplekitchenweb/project/app.js
@@ -0,0 +1,49 @@
+var myApp = angular.module('myApp', ['ngMessages']);
+
+myApp.controller('mainController', function ($scope, $filter, $timeout, $log){
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        $scope.$apply(function(){
+            if (request.readyState === 4 && request.status === 200) {
+                $scope.items = JSON.parse(request.responseText);
+                console.log($scope.items)
+            }});
+    };
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+    request.send();
+});
+
+myApp.controller('fifthLessonController', function ($scope, $filter, $timeout, $log){
+    $scope.handle = "";
+    $scope.charactersCount = 5;
+    $scope.rules = [
+        {ruleName: 'First'},
+        {ruleName: 'Second'},
+        {ruleName: 'Third'}
+    ]
+
+    $scope.lowerCaseHandle = function () {
+        return $filter('lowercase')($scope.handle);
+    };
+
+});
+
+myApp.controller('fourLessonController', function ($scope, $filter, $timeout, $log){
+    $scope.handle = "";
+    $scope.lowerCaseHandle = function () {
+        return $filter('lowercase')($scope.handle);
+    };
+
+    $scope.title = 'Four lesson'
+    $scope.name = 'Alex';
+    $scope.surname = 'Glover'
+    $scope.company = 'Oracle';
+
+    $timeout(function (){$scope.company = 'Microsoft';}, 3000);
+});
+
+myApp.controller('coolFieldController', function ($scope, $log, $filter){
+    $scope.thisIsTitle = "Third lesson"
+});
